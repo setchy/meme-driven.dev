5ffa82ee87651697dd3e7f54140d6a50fdd2422b
Adam Setch <adam.setch@outlook.com>
2024-11-19 07:33:34 -0500

    refactor: update best practices to support any file extension
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/memes/memes.ts
+++ src/memes/memes.ts
@@ -3,29 +3,29 @@ import type { BestPractice } from '~/types';
 export const bestPractices: BestPractice[] = [
   {
     title: 'Before Coding',
-    meme: 'distracted-boyfriend',
+    meme: 'distracted-boyfriend.jpg',
     caption:
       'Developer looking at "New Feature" while ignoring "Existing Bugs"',
   },
   {
     title: 'Pull Request Feedback',
-    meme: 'drake-hotline-bling',
+    meme: 'drake-hotline-bling.jpg',
     caption:
       'Drake rejecting "Messy Code" and approving "Clean, Refactored Code"',
   },
   {
     title: 'Daily Stand-up',
-    meme: 'hide-the-pain-harold',
+    meme: 'hide-the-pain-harold.jpg',
     caption: '"Finally fixed that bug!"',
   },
   {
     title: 'Retrospective',
-    meme: 'sus-fry',
+    meme: 'sus-fry.jpg',
     caption: '"Carrying over stories for the third sprint in a row"',
   },
   {
     title: 'Sprint Review',
-    meme: 'one-does-not-simply',
+    meme: 'one-does-not-simply.jpg',
     caption: 'Discussing "what is a story point" for the tenth time this month',
   },
 ];
